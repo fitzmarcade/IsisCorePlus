7dc0c3c2c8288b96d4484772c201b140f04f8bee
Fitz <markfitz@inbox.com>
2011-10-10 23:44:05 +0100

    Added lots of Temp. SQL's for Ahbot to TeleNPC, External Mail, Jail, GMIsland and Vendors. Just Added Wintergrasp for World_DB.

---
+++ sql/IsisCorePlus/Stormwind WMO.sql
@@ -0,0 +1,19 @@
+
+/*
+Stormwind WMO
+By Rochet2
+Downloaded from http://projectcode.zzl.org/
+Bugs and contact with E-mail: Rochet2@post.com
+*/
+
+DELETE FROM gameobject_template WHERE entry = '190002';
+
+INSERT INTO gameobject_template (entry, type, displayId, name, IconName, castBarCaption, unk1, faction, flags, size) 
+VALUES ('190002', 11, 471, "Stormwind WMO", '', '', '', 0, 0, 1);
+
+/*
+Stormwind WMO
+By Rochet2
+Downloaded from http://projectcode.zzl.org/
+Bugs and contact with E-mail: Rochet2@post.com
+*/
